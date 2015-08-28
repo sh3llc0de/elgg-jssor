c1b143eb9473ed968cd6e35365bdc0806280084e
Wade Benson <wade@cryptogeek.org>
2015-08-28 00:05:52 -0700

    update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -5,13 +5,18 @@ VER_MAJOR=1
 VER_MINOR=0
 VER_REV=$(cat version.txt)
 
+if [[ -d "${PKG_PATH}" ]]; then
+    rm -rf "${PKG_PATH}"
+fi
 mkdir -p ${PKG_PATH}
-git archive --format=tar HEAD | tar Cxf "${PKG_PATH}" -
-sed -i -e "s/VER_MAJOR/${VER_MAJOR}/g;s/VER_MINOR/${VER_MINOR}/g;s/VER_REV/${VER_REV}/g" ${PKG_PATH}/manifest.xml
-
-(($VER_REV++))
+git archive --format=tar --prefix="jssor"/ HEAD | tar Cxf "${PKG_PATH}" -
+((VER_REV++))
+sed -i "" -e "s/VER_MAJOR/${VER_MAJOR}/g;s/VER_MINOR/${VER_MINOR}/g;s/VER_REV/${VER_REV}/g" ${PKG_PATH}/jssor/manifest.xml
 echo "${VER_REV}" > version.txt
 git add version.txt
 git commit -m "pkg tag ${VER_MAJOR}.${VER_MINOR}.${VER_REV}"
 git tag "${VER_MAJOR}.${VER_MINOR}.${VER_REV}"
 #git push --all
+
+cd "${PKG_PATH}"
+zip -r "/tmp/jssor-${VER_MAJOR}.${VER_MINOR}.${VER_REV}.zip" jssor
